6975204dfe352f1cfa79217988ca16c56ceaf4c1
sakthi <sakthi12qaz@gmail.com>
2024-11-07 19:53:34 +0530

    yes

--- backend/settings.py
+++ backend/settings.py
@@ -87,15 +87,6 @@ STORAGES = {
     },
 }
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
